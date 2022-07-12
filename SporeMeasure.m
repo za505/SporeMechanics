3a71076ab2469346fa06fb3264a51d9e9e866011
za505 <za505@nyu.edu>
2022-07-12 15:33:54 -0400

    each experiment stored in a different element of the same cell array

--- SporeMeasure.m
+++ SporeMeasure.m
@@ -1,68 +1,125 @@
 %%SporeMeasure.m
 %Author: Zarina Akbary
 %Date: 30 June 2022
-%Purpose: to measure the long and short axis of B. subtilis 168 spores in
+%Purpose: to measure the long and short axis of B. subtilis PY79 spores in
 %isosmotic and hyperosmotic media
 
 clear, close all
 
 %User Input
-dirname = ['/Users/zarina/Downloads/NYU/20220627_Exp1'];
-basename_turgid = ['20220627_turgid']; 
-basename_hypershock = ['20220627_hypershock'];
-labels = {'isosmotic', 'hyperosmotic shock'};
+dirname = ['/Users/zarina/Downloads/EJProject'];
+basename = {'20220627_WT_turgid', '20220627_WT_hypershock', '20220707_cotXYZ_turgid', '20220707_cotXYZ_hypershock', '20220707_cotE_turgid', '20220707_cotE_hypershock'};
+labels = {'WT isosmotic', 'WT hypershock', 'cotE isosmotic', 'cotE hypershock', 'cotXYZ isosmotic', 'cotXYZ hypershock'};
 
-okabeIto = {[230, 159, 0], [86, 180, 233], [0, 158, 115], [0, 114, 178], [213, 94, 0], [204, 121, 167], [0, 0, 0]};
+%okabeIto = {[230, 159, 0], [86, 180, 233], [0, 158, 115], [0, 114, 178], [213, 94, 0], [204, 121, 167], [0, 0, 0]};
+okabeIto = {[230, 159, 0], [86, 180, 233], [0, 158, 115], [204, 121, 167], [213, 94, 0], [0, 114, 178], [0, 0, 0]};
 okabeIto = cellfun(@(x)(x./255), okabeIto, 'UniformOutput', false);
 okabeIto = [okabeIto, okabeIto];
 
 sm = 3; %parameter for edge detection
-dr=1;%Radius of dilation before watershed 
+dr=1;%Radius of dilation before watershed (water leaves cell) 
 lscale = 0.08; %Microns per pixel
 minA = 15; %minimum area for initial filter
 maxA = 70; %maximum area for final filter
 minF = 50; %minimum area for final filter
 maxF = 250;%maximum area for final filter
-troubleshooting = 1; %visualize cells
+troubleshooting = 0; %visualize cells
 
 %% load images and measure spores
-[B, centroids, DS, lcell_turgid, mlines, ncells_turgid, pcell, pixels, wcell_turgid] = sporeMeasure(dirname, basename_turgid, dr, lscale, maxA, maxF, minA, minF, sm, troubleshooting);
-[B, centroids, DS, lcell_hypershock, mlines, ncells_hypershock, pcell, pixels, wcell_hypershock] = sporeMeasure(dirname, basename_hypershock, dr, lscale, maxA, maxF, minA, minF, sm, troubleshooting);
+[boundaries, centroid, lCell, nCells, pixloc, wCell] = sporeMeasure(dirname, basename, dr, lscale, maxA, maxF, minA, minF, sm, troubleshooting);
 
-%% plot the dimensions
+%% plot the dimensions in scatter plot
+p = cell(length(basename),1);
+
 figure, hold on
-for i = 1:length(ncells_turgid)
-    p1 = scatter(lcell_turgid(i, :), wcell_turgid(i, :), 'MarkerFaceColor', okabeIto{1}, 'MarkerEdgeColor', okabeIto{1})
+for i = 1:length(basename) %bc using an array
+    [nrow, ncol] = size(lCell{i});
+    x = lCell{i};
+    y = wCell{i};
+    p{i} = scatter(lCell{i}, wCell{i}, 'MarkerFaceColor', okabeIto{i}, 'MarkerEdgeColor', okabeIto{i})
+end
+xlabel('axis 1 (\mum)')
+ylabel('axis 2 (\mum)')
+legend([p{1}(1), p{2}(1), p{3}(1), p{4}(1), p{5}(1), p{6}(1)], labels, 'Location', 'southeast')
+
+%% plot the dimensions as subplots
+p = cell(length(basename),1);
+j = 1;
+strain = {'', 'WT', '', 'cotE', '', 'cotXYZ'};
+
+figure('Units', 'normalized', 'outerposition', [0 0 1 1], 'DefaultAxesFontName', 'Arial', 'DefaultAxesFontSize', 20), hold on
+for i = 1:length(basename) %bc using an array
+    subplot(1, 3, j), hold on
+    [nrow, ncol] = size(lCell{i});
+    x = lCell{i};
+    y = wCell{i};
+    p{i} = scatter(lCell{i}, wCell{i}, 'MarkerFaceColor', okabeIto{i}, 'MarkerEdgeColor', okabeIto{i})
+    xlabel('axis 1 (\mum)')
+    ylabel('axis 2 (\mum)')
+  
+    if mod(i,2)==0
+        j = j+1;
+        legend([p{i-1}(1), p{i}(1)], {'Isosmotic', 'Hyperosmotic'}, 'Location', 'southeast')
+        title(strain{i})
+    end
 end
 
-for i = 1:length(ncells_hypershock)
-    p2 = scatter(lcell_hypershock(i, :), wcell_hypershock(i, :), 'MarkerFaceColor', okabeIto{2}, 'MarkerEdgeColor', okabeIto{2})
+%% Determine covariance between axis 1 and axis 2
+p = cell(length(basename),1);
+
+figure, hold on
+subplot(1,2,1), hold on
+for i = 1:length(basename) %bc using an array
+    [nrow, ncol] = size(lCell{i});
+    x1 = reshape(lCell{i}, [nrow*ncol, 1]);
+    p{i} = histogram(x1, 'EdgeColor', okabeIto{i}, 'FaceColor', okabeIto{i}, 'BinWidth', 0.2)
 end
-xlabel('Axis 1 (\mum)')
-ylabel('Axis 2 (\mum)')
-legend([p1(1), p2(1)], labels, 'Location', 'southeast')
+xlim([0.2, Inf])
+title('Axis 1 (\mum)')
+legend([p{1}(1), p{2}(1), p{3}(1), p{4}(1), p{5}(1), p{6}(1)], labels, 'Location', 'southeast')
+
+subplot(1,2,2), hold on
+for i = 1:length(basename) %bc using an array
+    [nrow, ncol] = size(lCell{i});
+    x2 = reshape(wCell{i}, [nrow*ncol, 1]);
+    p{i} = histogram(x2, 'EdgeColor', okabeIto{i}, 'FaceColor', okabeIto{i}, 'BinWidth', 0.2)
+end
+xlim([0.2, Inf])
+title('Axis 2 (\mum)')
+legend([p{1}(1), p{2}(1), p{3}(1), p{4}(1), p{5}(1), p{6}(1)], labels, 'Location', 'southeast')
 
 %% Functions
-function [B, centroids, DS, lcell, mlines, ncells, pcell, pixels, wcell] = sporeMeasure(dirname, basename, dr, lscale, maxA, maxF, minA, minF, sm, troubleshooting)
-    
-    cd([dirname '/' basename]);
-    directory = dir('*.tif');
-    N = length(directory);
+function [boundaries, centroid, lCell, nCells, pixloc, wCell] = sporeMeasure(dirname, basename, dr, lscale, maxA, maxF, minA, minF, sm, troubleshooting)
 
-    ncells = nan(N, 1);
-    centroids = cell(N,1);
-    pixels = cell(N,1);
-    B = cell(N,1);
-    pcell = nan(N, 1);
-    DS = nan(N, 1);
-    mlines = cell(N,1);
-    acell = nan(N, 1);
-    lcell = nan(N, 1);
-    wcell = nan(N, 1);
+    srow = length(basename);
+    boundaries = cell(srow, 1);
+    centroid = cell(srow, 1);
+    lCell = cell(srow, 1);
+    nCells = cell(srow,1);
+    pixloc = cell(srow,1);
+    wCell = cell(srow, 1);
+
+    for b = 1:srow
+
+        base = basename{b};
+        cd([dirname '/' base]);
+        directory = dir('*.tif'); %making directory of tif files
+        N = length(directory);
+
+        ncells = nan(N, 1); 
+        centroids = cell(N,1);
+        pixels = cell(N,1);
+        B = cell(N,1);
+        pcell = nan(N, 1);
+        DS = nan(N, 1);
+        mlines = cell(N,1);
+        acell = nan(N, 1);
+        lcell = nan(N, 1);
+        wcell = nan(N, 1);
 
     for n=1:N
 
-        imname = directory(n).name;
+        imname = directory(n).name; %changing tiff file name 
         im = imread(imname);
         [imM, imN] = size(im);
 
@@ -71,18 +128,18 @@ function [B, centroids, DS, lcell, mlines, ncells, pcell, pixels, wcell] = spore
 
         %De-speckle image
         im=medfilt2(im);
-    
+
         %Normalize images
-        ppix=0.5; %defauly 0.5
+        ppix=0.5; %default 0.5
         im=norm16bit(im,ppix);
 
         %figure(1), imshow(im)
         %figure(2), imhist(im), pause, close
 
         %enhance contrast
-        imc = im;
+        imc = im; %imc = de-speckled image
 
-        [imcounts,bins]=imhist(imc);
+        [imcounts,bins]=imhist(imc); 
         [imcounts,idx]=sort(imcounts);
         bins=bins(idx);
         thresh1=bins(end-1);
@@ -94,17 +151,17 @@ function [B, centroids, DS, lcell, mlines, ncells, pcell, pixels, wcell] = spore
 
         %Find edges
         [ed2,thresh2]=edge(imc,'canny',[],sm*sqrt(2));
-        
+
 %         [y, x] = find(ed2==1);
 %         figure(1)
 %         imshow(imc), hold on, plot(x,y,'.r'), pause, close
-        
+
         %Clean image
         cc=bwconncomp(ed2,8); %default 8
         stats=regionprops(cc,imc,'Area','MeanIntensity');
         idx=find([stats.Area]>minA&[stats.Area]<maxA);
         ed2=ismember(labelmatrix(cc),idx);
-        
+
         %Close gaps in edges
         despurred=bwmorph(ed2,'spur');
         spurs=ed2-despurred;
@@ -114,18 +171,18 @@ function [B, centroids, DS, lcell, mlines, ncells, pcell, pixels, wcell] = spore
             ed2(spy(k),spx(k))=1;
         end
         ed2=bwmorph(ed2,'bridge'); 
-            
+
         se=strel('disk',dr);
         ed2=imdilate(ed2,se);
         ed2=bwmorph(ed2,'thin',2);
-    
+
         %Identify cells based on size and intensity
         ed3=~ed2;
         ed3(1,:)=ones(1,imN);
         ed3(end,:)=ones(1,imN);
         ed3(:,1)=ones(imM,1);
         ed3(:,end)=ones(imM,1);
-              
+
         cc=bwconncomp(ed3,4);
         stats=regionprops(cc,imc,'Area','MeanIntensity');
 
@@ -136,11 +193,11 @@ function [B, centroids, DS, lcell, mlines, ncells, pcell, pixels, wcell] = spore
         bw=bwmorph(ed4,'thicken');
         [extrace,bw]=bwboundaries(bw,4,'noholes');
         stats=regionprops(bw, 'Area', 'Centroid','PixelIdxList');
-    
+
         %figure(1), imshow(bw), pause, close
-        
+
         ncells(n,1)=length(extrace);
-        
+
         %save pixel id list
         for k=1:ncells(n)
             acell(n,k)=stats(k).Area;
@@ -158,13 +215,13 @@ function [B, centroids, DS, lcell, mlines, ncells, pcell, pixels, wcell] = spore
 
             px=csaps(S,px,0.05,S);
             py=csaps(S,py,0.05,S);
-        
+
             px=px(sp+1:2*sp);
             py=py(sp+1:2*sp);
-        
+
             px(end)=px(1);
             py(end)=py(1);
-        
+
             dS=sqrt(diff(px).^2+diff(py).^2);
             S=[0 cumsum(dS)];
             ls=length(S);
@@ -172,21 +229,21 @@ function [B, centroids, DS, lcell, mlines, ncells, pcell, pixels, wcell] = spore
             Sn=(0:DS(n,k):S(end));
             nx=spline(S,px,Sn);
             ny=spline(S,py,Sn);
-        
+
             B{n,k}=[nx',ny'];
 
             X=B{n,k}(:,1);
             Y=B{n,k}(:,2);   
-                   
+
             [sX,~]=size(X);
-            
+
             %Find poles
             [X,Y,pcell(n,k)]=polefinder(X,Y);
 
             %Create mesh
             npts=min(pcell(n,k),sX-pcell(n,k)+1);
             S=(0:DS(n,k):(sX-1)*DS(n,k));
-            
+
             s1=(0:S(pcell(n,k))/(npts-1):S(pcell(n,k)));
             s2=(S(pcell(n,k)):(S(end)-S(pcell(n,k)))/(npts-1):S(end));
             xc1=spline(S(1:pcell(n,k)),X(1:pcell(n,k)),s1);
@@ -201,17 +258,17 @@ function [B, centroids, DS, lcell, mlines, ncells, pcell, pixels, wcell] = spore
             dxy=diff(mlines{n,k}).^2;
             dl=sqrt(dxy(:,1)+dxy(:,2));
             lcell(n,k)=sum(dl);
-                            
+
             %Calculate width
             ls=[0 cumsum(dl)'];
             [~,mpos1]=min(abs(ls/lcell(n,k)-0.25));
             [~,mpos2]=min(abs(ls/lcell(n,k)-0.75));
-            
+
             widths=sqrt((xc1-xc2).^2+(yc1-yc2).^2);
             wcell(n,k)=(widths(mpos1)+widths(mpos2))/2;
-            
+
         end
-        
+
         %Dimsionalize the variables
         lcell(n,:)=lcell(n, :)*lscale;
         wcell(n,:)=wcell(n, :)*lscale;
@@ -238,6 +295,13 @@ function [B, centroids, DS, lcell, mlines, ncells, pcell, pixels, wcell] = spore
         end
 
     end
+
+    boundaries{b} = B;
+    centroid{b} = centroids;
+    lCell{b} = lcell;
+    nCells{b} = ncells;
+    pixloc{b} = pixels;
+    wCell{b} = wcell;
+
+    end
 end
-
-
